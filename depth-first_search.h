b56a22a4355ef020e3b3f952459e5dd4b98e4577
C0DE <64311703+KOD-ex@users.noreply.github.com>
2020-08-24 18:45:43 +0000

    Delete depth-first_search.h

--- depth-first_search.h
+++
@@ -1,8 +0,0 @@
-//проверка вершины 
-int check_vertex(int vertex);
-
-//поиск в ширину
-int depth_first_search(int graph[][6], int vertex);
-
-//проверка что вершина не была проверена
-int check_vertex_transition(int visited_vertices[], int current_vertex);
